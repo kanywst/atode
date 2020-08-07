ce8289445f2ff04bc90e2edcea65f30c016a418f
kanywst <niwatakuma@icloud.com>
2020-08-07 14:28:42 +0900

    Add url validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,6 @@ class Micropost < ApplicationRecord
   default_scope -> {order(created_at: :desc)}
   validates :user_id,presence: true
   validates :title,presence: true
-  validates :url,presence: true
+  VALID_URL_REGEX = /\A#{URI::regexp(%w(http https))}\z/i
+  validates :url,presence: true,format: {with: VALID_URL_REGEX}
 end
