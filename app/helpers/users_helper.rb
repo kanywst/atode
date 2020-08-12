ecf15afd2a495b6647941ef4f064db5596a0214a
kanywst <niwatakuma@icloud.com>
2020-08-12 20:41:49 +0900

    delete profile image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,2 @@
 module UsersHelper
-
-    # 渡されたユーザーのGravatar画像を返す
-    def gravatar_for(object, options = { size: 80 })
-      gravatar_id = Digest::MD5::hexdigest(SecureRandom.hex(8))
-      size = options[:size]
-      gravatar_url = object.image? ? object.image.thumb.url : "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-      image_tag(gravatar_url, alt: object.name, class: "gravatar")
-    end
-  end
+end
