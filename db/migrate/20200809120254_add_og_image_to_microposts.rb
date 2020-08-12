517b6fc3014fca71b084ef070e9030d3a7e42063
kanywst <niwatakuma@icloud.com>
2020-08-12 22:05:48 +0900

    Removing all files in .gitignore

--- db/migrate/20200809120254_add_og_image_to_microposts.rb
+++
@@ -1,5 +0,0 @@
-class AddOgImageToMicroposts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :microposts, :og_image, :string
-  end
-end
--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-User.create!(username: "unko",
-             name:  "Unko",
-             email: "example@railstutorial.org",
-             password:              "qXxTJU+SVdFD#C$hF7qd",
-             password_confirmation: "qXxTJU+SVdFD#C$hF7qd",
-             admin: true)
